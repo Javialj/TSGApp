39d58747a7a9793d8e1bf5dd619a5ac8d096ee8d
Javialj <javialj12@gmail.com>
2025-03-29 15:38:16 +0100

    Gradle modificado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.tsgapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.tsgapp"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
