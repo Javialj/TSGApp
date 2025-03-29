0ad0af1e1ddaf49bc7aca50d746406aa477e4f14
Javialj <javialj12@gmail.com>
2025-03-29 17:27:17 +0100

    Ventana usuario

--- app/src/main/java/com/example/tsgapp/ActivityVentanaUsuarios.kt
+++ app/src/main/java/com/example/tsgapp/ActivityVentanaUsuarios.kt
@@ -1,2 +1,129 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.tsgapp
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class ActivityVentanaUsuarios : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VentanaUsuarios()
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun VentanaUsuarios(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(16.dp),  // Opcional: margen para evitar pegarse a los bordes
+        horizontalAlignment = Alignment.CenterHorizontally,  // Centrado horizontal
+        verticalArrangement = Arrangement.Top
+    ) {
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(
+                    text = "Usuario:",
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.Bold
+                )
+            },
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = {
+                Text(
+                    text = "Contraseña:",
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.Bold
+                )
+            },
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Divider(
+                modifier = Modifier.weight(1f),
+                color = Color.Gray,
+                thickness = 1.dp
+            )
+
+            Text(
+                text = "or",
+                fontSize = 18.sp, // Tamaño personalizado
+                fontWeight = FontWeight.Medium,
+                modifier = Modifier.padding(horizontal = 8.dp),
+                color = Color.Gray
+            )
+
+            Divider(
+                modifier = Modifier.weight(1f),
+                color = Color.Gray,
+                thickness = 1.dp
+            )
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(
+            onClick = { /* Acción al hacer clic */ },
+            modifier = Modifier.fillMaxWidth()
+        ) { }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /* Acción al hacer clic */ },
+            modifier = Modifier.fillMaxWidth()
+        ) { }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /* Acción al hacer clic */ },
+            modifier = Modifier.fillMaxWidth()
+        ) { }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun VentanaUsuariosPreview() {
+    VentanaUsuarios()
+}
