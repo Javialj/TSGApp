7491a0c1e43051dd526570d57602c1f77304d3ab
Javialj <javialj12@gmail.com>
2025-04-06 16:07:12 +0200

    "Logos, VentanaUsuarios y VentanaPrincipal 06-04-25"

--- app/src/main/java/com/example/tsgapp/ActivityVentanaUsuarios.kt
+++ app/src/main/java/com/example/tsgapp/ActivityVentanaUsuarios.kt
@@ -5,15 +5,18 @@ package com.example.tsgapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,7 +27,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,8 +50,8 @@ fun VentanaUsuarios(modifier: Modifier = Modifier) {
     Column(
         modifier
             .fillMaxSize()
-            .padding(16.dp),  // Opcional: margen para evitar pegarse a los bordes
-        horizontalAlignment = Alignment.CenterHorizontally,  // Centrado horizontal
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
         OutlinedTextField(
@@ -60,7 +65,9 @@ fun VentanaUsuarios(modifier: Modifier = Modifier) {
                 )
             },
         )
+
         Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(
             value = "",
             onValueChange = {},
@@ -71,6 +78,14 @@ fun VentanaUsuarios(modifier: Modifier = Modifier) {
                     fontWeight = FontWeight.Bold
                 )
             },
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(
+            text = "Olvidaste tu contraseña?",
+            fontSize = 18.sp,
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue
         )
         Spacer(modifier = Modifier.height(30.dp))
 
@@ -86,7 +101,7 @@ fun VentanaUsuarios(modifier: Modifier = Modifier) {
 
             Text(
                 text = "or",
-                fontSize = 18.sp, // Tamaño personalizado
+                fontSize = 18.sp,
                 fontWeight = FontWeight.Medium,
                 modifier = Modifier.padding(horizontal = 8.dp),
                 color = Color.Gray
@@ -103,22 +118,109 @@ fun VentanaUsuarios(modifier: Modifier = Modifier) {
 
         Button(
             onClick = { /* Acción al hacer clic */ },
-            modifier = Modifier.fillMaxWidth()
-        ) { }
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Gmail",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.gmail),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(24.dp)
+                        .align(Alignment.CenterStart)
+                )
+            }
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Button(
             onClick = { /* Acción al hacer clic */ },
-            modifier = Modifier.fillMaxWidth()
-        ) { }
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Google",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.google),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(Alignment.CenterStart)
+            )
+        }
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
         Button(
             onClick = { /* Acción al hacer clic */ },
-            modifier = Modifier.fillMaxWidth()
-        ) { }
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "X",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.twitter),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(24.dp)
+                        .align(Alignment.CenterStart)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { /* Acción al hacer clic */ },
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Facebook",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Image(
+                    painter = painterResource(id = R.drawable.facebook),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(24.dp)
+                        .align(Alignment.CenterStart)
+                )
+            }
+        }
     }
 }
 
